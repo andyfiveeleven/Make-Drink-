eb57ab9ba2356ff9183a4a69840eb3a7ad0bd698
sharmarke1 <sharmarkeibrahim@Sharmarke-Ibrahims-MacBook-Pro.local>
2017-05-29 11:55:07 -0700

    added img to html and fixed casing

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var displayList = [];
 var whiskey = document.getElementById('whiskey');
 var vodka = document.getElementById('vodka');
 var gin = document.getElementById('gin');
-var lightRum = document.getElementById('light-rum');
-var darkRum = document.getElementById('dark-rum');
+var lightRum = document.getElementById('lightRum');
+var darkRum = document.getElementById('darkRum');
 var tequila = document.getElementById('tequila');
 var champagne = document.getElementById('champagne');
-var darkBeer = document.getElementById('dark-beer');
-var lightBeer = document.getElementById('light-beer');
-var sweetVermouth = document.getElementById('sweet-vermouth');
+var darkBeer = document.getElementById('darkBeer');
+var lightBeer = document.getElementById('lightBeer');
+var sweetVermouth = document.getElementById('sweetVermouth');
 var bitters = document.getElementById('bitters');
-var dryVermouth = document.getElementById('dry-vermouth');
+var dryVermouth = document.getElementById('dryVermouth');
 var brandy = document.getElementById('brandy');
-var orangeLiquer = document.getElementById('orange-liqueur');
+var orangeLiquer = document.getElementById('orangeLiqueur');
 var campari = document.getElementById('campari');
 var sugar = document.getElementById('sugar');
 var limes = document.getElementById('limes');
@@ -22,7 +22,7 @@ var lemons = document.getElementById('lemons');
 var oranges = document.getElementById('oranges');
 var grenadine = document.getElementById('grenadine');
 var cranberry = document.getElementById('cranberry');
-var clubSoda = document.getElementById('club-soda');
+var clubSoda = document.getElementById('clubSoda');
 
 var goBtn = document.getElementById('go');
 var backBtn = document.getElementById('back');
@@ -37,39 +37,39 @@ function Cocktail(name, ingredients, recLink) {
   cocktails.push(this);
 }
 
-new Cocktail('Manhattan', ['whiskey','sweet-vermouth', 'bitters'], 'http://www.seriouseats.com/recipes/2008/01/cocktails-manhattan-recipe.html');
+new Cocktail('Manhattan', ['whiskey','sweetVermouth', 'bitters'], 'http://www.seriouseats.com/recipes/2008/01/cocktails-manhattan-recipe.html');
 
 new Cocktail('Old Fashioned' , ['whiskey', 'sugar', 'bitters'], 'http://www.seriouseats.com/recipes/2008/06/traditional-old-fashioned-recipe.html');   //extra? orange
 
-new Cocktail('Martinez', ['gin', 'sweet-vermouth', 'bitters'], 'http://www.seriouseats.com/recipes/2011/09/martinez-cocktail-gin-cocktail-recipe.html');
+new Cocktail('Martinez', ['gin', 'sweetVermouth', 'bitters'], 'http://www.seriouseats.com/recipes/2011/09/martinez-cocktail-gin-cocktail-recipe.html');
 
-new Cocktail('martini', ['dry-vermouth', 'gin', 'orange-bitters'], 'http://www.seriouseats.com/recipes/2010/06/the-martini-recipe.html');
+new Cocktail('martini', ['dryVermouth', 'gin', 'bitters'], 'http://www.seriouseats.com/recipes/2010/06/the-martini-recipe.html');
 
-new Cocktail('brooklyn', ['whiskey', 'dry-vermouth', 'bitters'], 'http://www.seriouseats.com/recipes/2011/02/the-brooklyn-cocktail-rye-maraschino-vermouth.html');
+new Cocktail('brooklyn', ['whiskey', 'dryVermouth', 'bitters'], 'http://www.seriouseats.com/recipes/2011/02/the-brooklyn-cocktail-rye-maraschino-vermouth.html');
 
-new Cocktail('daiquiri', ['light-rum', 'lime', 'sugar'], 'http://www.seriouseats.com/recipes/2008/05/daiquiri-recipe.html');
+new Cocktail('daiquiri', ['lightRum', 'limes', 'sugar'], 'http://www.seriouseats.com/recipes/2008/05/daiquiri-recipe.html');
 
-new Cocktail('margarita', ['tequila', 'lime', 'orange-liqueur'], 'http://www.seriouseats.com/recipes/2015/04/classic-margarita-recipe-tequila-cocktail.html');
+new Cocktail('margarita', ['tequila', 'limes', 'orangeLiqueur'], 'http://www.seriouseats.com/recipes/2015/04/classic-margarita-recipe-tequila-cocktail.html');
 
-new Cocktail('side car', ['brandy', 'lemon', 'orange-liqueur'], 'http://www.seriouseats.com/recipes/2010/11/sidecar-drink-cocktail-recipe.html');
+new Cocktail('side car', ['brandy', 'lemons', 'orangeLiqueur'], 'http://www.seriouseats.com/recipes/2010/11/sidecar-drink-cocktail-recipe.html');
 
-new Cocktail('french 75', ['gin', 'champagne', 'lemon', 'sugar'], 'http://www.seriouseats.com/recipes/2011/03/french-75-cocktial-gin-champagne-lemon.html');
+new Cocktail('french 75', ['gin', 'champagne', 'lemons', 'sugar'], 'http://www.seriouseats.com/recipes/2011/03/french-75-cocktial-gin-champagne-lemon.html');
 
-new Cocktail('negroni', ['gin', 'sweet-vermouth', 'campari'], 'http://www.seriouseats.com/recipes/2010/04/negroni-cocktail-recipe-gin-campari-vermouth.html');
+new Cocktail('negroni', ['gin', 'sweetVermouth', 'campari'], 'http://www.seriouseats.com/recipes/2010/04/negroni-cocktail-recipe-gin-campari-vermouth.html');
 
-new Cocktail('boulevardier', ['whiskey', 'campari', 'sweet-vermouth'], 'http://www.seriouseats.com/recipes/2008/09/boulevardier-recipe.html');
+new Cocktail('boulevardier', ['whiskey', 'campari', 'sweetVermouth'], 'http://www.seriouseats.com/recipes/2008/09/boulevardier-recipe.html');
 
-new Cocktail('vieux carre', ['bitters', 'whiskey', 'brandy', 'sweet-vermouth'], 'http://www.seriouseats.com/recipes/2008/07/vieux-carre-recipe.html');
+new Cocktail('vieux carre', ['bitters', 'whiskey', 'brandy', 'sweetVermouth'], 'http://www.seriouseats.com/recipes/2008/07/vieux-carre-recipe.html');
 
-new Cocktail('whiskey sour', ['whiskey', 'lemon', 'sugar'], 'http://www.seriouseats.com/recipes/2007/10/cocktails-whiskey-sour.html');
+new Cocktail('whiskey sour', ['whiskey', 'lemons', 'sugar'], 'http://www.seriouseats.com/recipes/2007/10/cocktails-whiskey-sour.html');
 
-new Cocktail('planters punch', ['dark rum', 'sugar', 'lime', 'bitters'], 'http://www.seriouseats.com/recipes/2010/07/planters-punch-cocktail-recipe.html');
+new Cocktail('planters punch', ['darkRum', 'sugar', 'limes', 'bitters'], 'http://www.seriouseats.com/recipes/2010/07/planters-punch-cocktail-recipe.html');
 
-new Cocktail('cosmopolitan', ['vodka', 'orange liqueur', 'cranberry', 'lime'], 'http://www.seriouseats.com/recipes/2011/10/how-to-make-a-cosmopolitan-cocktail-best-recipe.html');
+new Cocktail('cosmopolitan', ['vodka', 'orangeLiqueur', 'cranberry', 'limes'], 'http://www.seriouseats.com/recipes/2011/10/how-to-make-a-cosmopolitan-cocktail-best-recipe.html');
 
-new Cocktail('vodka cranberry', ['vodka', 'cranberry', 'club soda', 'lime'], 'https://www.tablespoon.com/recipes/fresh-cranberry-vodka-cocktail/f8de4d8e-d91a-4dbb-b701-9f927428f0a7');
+new Cocktail('vodka cranberry', ['vodka', 'cranberry', 'clubSoda', 'limes'], 'https://www.tablespoon.com/recipes/fresh-cranberry-vodka-cocktail/f8de4d8e-d91a-4dbb-b701-9f927428f0a7');
 
-new Cocktail('tom collins', ['gin', 'lemon', 'sugar', 'club soda'], 'http://www.seriouseats.com/recipes/2008/08/tom-collins-recipe.html');
+new Cocktail('tom collins', ['gin', 'lemons', 'sugar', 'clubSoda'], 'http://www.seriouseats.com/recipes/2008/08/tom-collins-recipe.html');
 
 function imgClick(e) {
   var clickedImg = e.target.getAttribute('id');
@@ -87,12 +87,15 @@ function goClick() {
   options.style.display = 'none';
   canMake.style.display = '';
   results();
+  // console.log('userChoices:', userChoices);
 }
 
 function checker() {
   cocktails.forEach(function(drink) {
     if (containsAll(drink)) {
       displayList.push(drink);
+      console.log('displayList:',displayList);
+      console.log('drink:', drink);
     }
   });
 }
@@ -136,5 +139,24 @@ canMake.style.display = 'none';
 whiskey.addEventListener('click', imgClick);
 vodka.addEventListener('click', imgClick);
 gin.addEventListener('click', imgClick);
+lightRum.addEventListener('click', imgClick);
+darkRum.addEventListener('click', imgClick);
+tequila.addEventListener('click', imgClick);
+champagne.addEventListener('click', imgClick);
+darkBeer.addEventListener('click', imgClick);
+lightBeer.addEventListener('click', imgClick);
+sweetVermouth.addEventListener('click', imgClick);
+bitters.addEventListener('click', imgClick);
+dryVermouth.addEventListener('click', imgClick);
+brandy.addEventListener('click', imgClick);
+orangeLiquer.addEventListener('click', imgClick);
+campari.addEventListener('click', imgClick);
+sugar.addEventListener('click', imgClick);
+limes.addEventListener('click', imgClick);
+lemons.addEventListener('click', imgClick);
+oranges.addEventListener('click', imgClick);
+grenadine.addEventListener('click', imgClick);
+cranberry.addEventListener('click', imgClick);
+clubSoda.addEventListener('click', imgClick);
 goBtn.addEventListener('click', goClick);
 backBtn.addEventListener('click', backClick);
